9dfab83e6e91ad6570c3262c250b95b5a14e826d
Andryteck <772316andrew@gmail.com>
2020-07-14 22:20:19 +0300

    Updates

---
+++ precache-manifest.67dba1d00a95e4a2a9a1215f101fc4ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20746ea83d609ae270a9aaae0cfcbf6e",
+    "url": "/react-way-of-samurai/index.html"
+  },
+  {
+    "revision": "363222ae2e1b661d68a1",
+    "url": "/react-way-of-samurai/static/css/main.4beec3ff.chunk.css"
+  },
+  {
+    "revision": "3883562dfa4c781137ee",
+    "url": "/react-way-of-samurai/static/js/2.04c14f82.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-way-of-samurai/static/js/2.04c14f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363222ae2e1b661d68a1",
+    "url": "/react-way-of-samurai/static/js/main.4c7a83b7.chunk.js"
+  },
+  {
+    "revision": "51f4eea39946cd423b81",
+    "url": "/react-way-of-samurai/static/js/runtime-main.0577ae31.js"
+  },
+  {
+    "revision": "bd62b284202d2ace3e0e956e3c22917d",
+    "url": "/react-way-of-samurai/static/media/preLouder.bd62b284.svg"
+  }
+]);
