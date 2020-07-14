df0950e11d975f99fe36ea5c43f67453492615f4
Andryteck <772316andrew@gmail.com>
2020-07-14 23:56:58 +0300

    feat:add hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/redux-store'
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 
     ReactDOM.render(
         <React.StrictMode>
-            <BrowserRouter>
+            <HashRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-            </BrowserRouter>
+            </HashRouter>
         </React.StrictMode>
         ,
         document.getElementById('root')
